5f9318f2a561ce0fcc589ffdd73350b3e288badf
JazzKr <codeex@insomniacraft.net>
2012-01-20 20:14:47 -0500

    Added delayed payment & broadcast

--- src/Vote4CashListener.java
+++ src/Vote4CashListener.java
@@ -5,40 +5,73 @@ import com.vexsoftware.votifier.model.VoteListener;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 import java.util.Properties;
 import java.util.logging.Logger;
 import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.economy.EconomyResponse;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerListener;
 import org.bukkit.plugin.RegisteredServiceProvider;
 
-public class Vote4CashListener implements VoteListener {
+public class Vote4CashListener extends PlayerListener implements VoteListener {
 	private static final Logger log = Logger.getLogger("Vote4CashListener");
 
 	private static Votifier v = null;
 	private static Economy econ = null;
 
+	private static boolean broadcast;
 	private static double reward;
 	private static String msg;
+	private static String broadcastMsg;
 	private static String currencyS;
 	private static String currencyP;
 
 	private static File dataFolder;
 	private static File propertiesFile;
+	private static File dataFile;
+	
+	private static ArrayList<String> pending;
 
+	@SuppressWarnings("unchecked")
 	public Vote4CashListener() {
+		
 		v = Votifier.getInstance();
+		
+		//Set up all files
+		dataFolder = v.getDataFolder();
+		propertiesFile = new File(dataFolder, "Vote4Cash.properties");
+		dataFile = new File(dataFolder, "V4C.data");
+		
+		//Create a new data file with an empty data array in it if there isn't one already
+		if (!dataFile.exists()) {
+			try {
+				ArrayList<String> empty = new ArrayList<String>();
+				ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(dataFile));
+				oos.writeObject(empty);
+				oos.close();
+			}
+			catch(Exception e) {
+				log.info("[Vote4Cash] Error creating data file! Delayed payment to players may not work.");
+			}
+		}
 
 		if (v != null) {
 			Properties pro = new Properties();
-			dataFolder = v.getDataFolder();
-			propertiesFile = new File(dataFolder, "Vote4Cash.properties");
 			
 			//Create properties file
 			if (!propertiesFile.exists()) {
 				pro.setProperty("reward", "50");
-				pro.setProperty("player-voted-msg", "Thank you for voting %PLAYER%! To show our appreciation here is %REWARD% %CURRENCY%!");
+				pro.setProperty("player-voted-msg", "[Vote4Cash] Thank you for voting %PLAYER%! To show our appreciation here is %REWARD% %CURRENCY%!");
+				pro.setProperty("broadcast-msg", "[Vote4Cash] %PLAYER% has voted and received %REWARD% %CURRENCY%. Thank you %PLAYER%!");
 				pro.setProperty("currency-name-singular", "dollar");
 				pro.setProperty("currency-name-plural", "dollars");
+				pro.setProperty("broadcast","true");
 				try {
 					pro.store(new FileOutputStream(propertiesFile), "Vote4Cash Properties");
 				} catch (Exception e) {
@@ -51,8 +84,10 @@ public class Vote4CashListener implements VoteListener {
 				pro.load(new FileInputStream(propertiesFile));
 				reward = Double.parseDouble(pro.getProperty("reward"));
 				msg = pro.getProperty("player-voted-msg");
+				broadcastMsg = pro.getProperty("broadcast-msg");
 				currencyS = pro.getProperty("currency-name-singular");
 				currencyP = pro.getProperty("currency-name-plural");
+				broadcast = Boolean.parseBoolean(pro.getProperty("broadcast"));
 			} catch (Exception e) {
 				log.severe("[Vote4Cash] Error reading existing properties file!");
 			}
@@ -64,24 +99,46 @@ public class Vote4CashListener implements VoteListener {
 			} else {
 				log.severe("[Vote4Cash] Could not find Vault! Vote4Cash Listener will not work!");
 			}
+			
+			//Load pending players from data file
+			pending = new ArrayList<String>();
+			try {
+				ObjectInputStream ois = new ObjectInputStream(new FileInputStream(dataFile));
+				pending = (ArrayList<String>) ois.readObject();
+				ois.close();
+			} catch (Exception e) {
+				log.severe("[Vote4Cash] Error reading data file! Delayed payment will probably not work.");
+			}
+			
+			//Register a player listener to do things on player log in
+			v.getServer().getPluginManager().registerEvent(Event.Type.PLAYER_JOIN, this, Event.Priority.Normal, v);			
 		}
 	}
 
 	public void voteMade(Vote vote) {
 		if (econ != null) {
-			String player = vote.getUsername();
-			if (v.getServer().getPlayer(player) != null) {
-				EconomyResponse r = econ.depositPlayer(player, reward);
-				if (r.transactionSuccess()) {
-					v.getServer().getPlayer(player).sendMessage(formatOutput(msg,player));
-					log.info("[Vote4Cash] " + player + " just voted and received "+reward+" "+(reward>1?currencyP:currencyS)+".");
-				} else {
-					v.getServer().getPlayer(player).sendMessage("Error giving money:" + r.errorMessage);
-					log.info("[Vote4Cash] " + player + " just voted but money was not given. Here is the error: " + r.errorMessage);
+			String player = vote.getUsername();	
+			
+			//Check if they are on server for instant payment, otherwise put on waiting list if not already on it
+			Player[] players = v.getServer().getOnlinePlayers();
+			for (int i = 0; i < players.length; i++) {
+				String playerName = players[i].getName();
+				if (player.equalsIgnoreCase(playerName)) {
+					pay(players[i]);
 				}
-			} else {
-				log.info("[Vote4Cash] " + player + " was not found on the server, therefore could not be given money.");
 			}
+			
+			//If they got this far they are not on server so check if they are already on pending list
+			for (int i = 0; i < pending.size(); i++) {
+				if (pending.get(i).equals(player)) {
+					log.info("[Vote4Cash] "+ player + " is not on the server but is already on the waiting list to receive money, will not submit again.");
+				}
+			}
+			
+			//Now that they are not on the pending list or on the server, make an entry for them in the pending list!
+			log.info("[Vote4Cash] " + player + " was not found on the server, so he will be given money on next login.");
+			pending.add(player);
+			save(pending);
 		}
 	}
 	
@@ -106,5 +163,47 @@ public class Vote4CashListener implements VoteListener {
 			returnS = returnS + split[i];
 		}
 		return returnS;
+	}
+	
+	public void onPlayerJoin(PlayerJoinEvent e) {
+		if (pending.isEmpty()) {
+			return;
+		}
+		
+		for (int i = 0; i < pending.size(); i++) {
+			String player = e.getPlayer().getName();
+			if (player.equals(pending.get(i))) {
+				log.info("[Vote4Cash] Found "+player+" in pending list. Paying now!");
+				pay(e.getPlayer());
+				pending.remove(i);
+				save(pending);
+			}			
+		}
+	}
+	
+	public void pay(Player player) {
+				EconomyResponse r = econ.depositPlayer(player.getName(), reward);
+				if (r.transactionSuccess()) {
+					player.sendMessage(formatOutput(msg,player.getName()));
+					log.info("[Vote4Cash] " + player.getName() + " has just received "+reward+" "+(reward>1?currencyP:currencyS)+" for voting.");
+					if (broadcast)
+						v.getServer().broadcastMessage(formatOutput(broadcastMsg,player.getName()));
+				} else {
+					player.sendMessage("Error giving money:" + r.errorMessage);
+					log.info("[Vote4Cash] " + player.getName() + " could not be given money for voting. Here is the error: " + r.errorMessage);
+				}
+	}
+	
+	public void save(ArrayList<String> al) {
+		if (dataFile.exists()) {
+			dataFile.delete();
+		}		
+		try {
+			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(dataFile));
+			oos.writeObject(al);
+			oos.close();
+		} catch (Exception e) {
+			log.severe("[Vote4Cash] Error saving list of pending players!");
+		}
 	}
 }
